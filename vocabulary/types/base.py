728af29bbabc8dd41f024915c8b4279d0e2a59d3
amoh-godwin <amohgyebigodwin@gmail.com>
2019-10-30 00:44:03 +0000

    Adds more types

--- vocabulary/types/base.py
+++ vocabulary/types/base.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 
-base_types = ['if', 'for']
+base_types = ['True', 'False', 'None', 'if', 'elif', 'else', 'for', 'while',
+              'class', 'def', 'with', 'as', 'in', 'import', 'from']
 
-base_types_dict = {'if': "<span>if</span>","for": "<span>for</span>"}
+base_functions = ['print', 'list', 'str', 'bytes', 'dict', 'set', 'tuple',
+                  'len', 'format', 'open']
+
+base_types_dict = {'True': "<span>True</span>", 'False': "<span>False</span>",
+                   'None': "<span>None</span>",'if': "<span>if</span>",
+                   'elif': "<span>elif</span>", 'else': "<span>else</span>",
+                   "for": "<span>for</span>", 'while': "<span>while</span>",
+                   'class': "<span>class</span>", 'def': "<span>def</span>",
+                   'with': "<span>with</span>", 'as': "<span>as</span>",
+                   'in': "<span>in</span>", 'import': "<span>import</span>",
+                   'from': "<span>from</span>"}
+
+base_func_dict = {'print': "<span>print</span>", 'list': "<span>list</span>",
+                  'str': "<span>str</span>", 'bytes': "<span>bytes</span>",
+                  'dict': "<span>dict</span>", 'set': "<span>set</span>",
+                  'tuple': "<span>tuple</span>", 'len': "<span>len</span>",
+                  'format': "<span>format</span>", 'open': "<span>open</span>"}
+
