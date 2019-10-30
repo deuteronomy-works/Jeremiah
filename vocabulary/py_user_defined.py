ccbb3887b64382472961970d3fb3732d81f95ca2
amoh-godwin <amohgyebigodwin@gmail.com>
2019-10-30 14:01:39 +0000

    read all functions in the file

--- vocabulary/py_user_defined.py
+++ vocabulary/py_user_defined.py
@@ -6,4 +6,25 @@ class UserDefined():
         self.content = content
 
     def start(self):
+        self._find_functions()
         return self.content
+
+    def _find_functions(self):
+        # read in file
+        lines = self.content.split('\r\n')
+        no = -1
+        for line in lines:
+            no += 1
+            if 'def ' in line:
+                name, values = self._parse_function(line)
+
+    def _pares_function(self, line):
+        splits = line.split('(')
+        name = splits[0].split(' ')[1]
+        value_str = splits[1].split(')')[0]
+        if ' ' in value_str:
+            values = value_str.split(', ')
+        else:
+            values = value_str.split(',')
+
+        return name, values
