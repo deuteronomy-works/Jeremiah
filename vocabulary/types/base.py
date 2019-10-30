ae39894526e3f8cd92f80de08472510e86307567
amoh-godwin <amohgyebigodwin@gmail.com>
2019-10-30 00:57:46 +0000

    Add opening braces to base functions

--- vocabulary/types/base.py
+++ vocabulary/types/base.py
@@ -3,8 +3,8 @@
 base_types = ['True', 'False', 'None', 'if', 'elif', 'else', 'for', 'while',
               'class', 'def', 'with', 'as', 'in', 'import', 'from']
 
-base_functions = ['print', 'list', 'str', 'bytes', 'dict', 'set', 'tuple',
-                  'len', 'format', 'open']
+base_functions = ['print(', 'list(', 'str(', 'bytes(', 'dict(', 'set(', 'tuple(',
+                  'len(', 'format(', 'open(']
 
 base_types_dict = {'True': "<span>True</span>", 'False': "<span>False</span>",
                    'None': "<span>None</span>",'if': "<span>if</span>",
@@ -15,9 +15,9 @@ base_types_dict = {'True': "<span>True</span>", 'False': "<span>False</span>",
                    'in': "<span>in</span>", 'import': "<span>import</span>",
                    'from': "<span>from</span>"}
 
-base_func_dict = {'print': "<span>print</span>", 'list': "<span>list</span>",
-                  'str': "<span>str</span>", 'bytes': "<span>bytes</span>",
-                  'dict': "<span>dict</span>", 'set': "<span>set</span>",
-                  'tuple': "<span>tuple</span>", 'len': "<span>len</span>",
-                  'format': "<span>format</span>", 'open': "<span>open</span>"}
+base_func_dict = {'print(': "<span>print</span>(", 'list(': "<span>list</span>(",
+                  'str(': "<span>str</span>(", 'bytes(': "<span>bytes</span>(",
+                  'dict(': "<span>dict</span>(", 'set(': "<span>set</span>(",
+                  'tuple(': "<span>tuple</span>(", 'len(': "<span>len</span>(",
+                  'format(': "<span>format</span>(", 'open(': "<span>open</span>("}
 
