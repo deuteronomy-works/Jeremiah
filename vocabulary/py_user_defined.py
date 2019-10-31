715b469e85ce5dbd1c597465d11104ec2912ad8d
amoh-godwin <amohgyebigodwin@gmail.com>
2019-10-31 01:49:04 +0000

    read functions also in classes

--- vocabulary/py_user_defined.py
+++ vocabulary/py_user_defined.py
@@ -3,28 +3,70 @@
 class UserDefined():
 
     def __init__(self, content):
+        self.indent = 4
         self.content = content
+        self.classes_parent = {}
+        self.functions_parent = {'__file__': []}
+        self.functions = []
 
     def start(self):
-        self._find_functions()
+        self._findall()
+        print(self.functions_parent)
         return self.content
 
-    def _find_functions(self):
-        # read in file
+    def _findall(self):
+
         lines = self.content.split('\r\n')
         no = -1
         for line in lines:
             no += 1
-            if 'def ' in line:
+            if 'class ' in line:
+                self._parse_classes(line)
+            elif 'def ' in line:
                 name, values = self._parse_function(line)
 
-    def _pares_function(self, line):
-        splits = line.split('(')
-        name = splits[0].split(' ')[1]
-        value_str = splits[1].split(')')[0]
+    def _parse_classes(self, line):
+        splits = line.split('class ')
+        spaces = splits[0]
+        remain = splits[1]
+        if '(' in remain:
+            name = remain.split('(')[0]
+        elif ' :' in remain:
+            name = remain.split(' :')[0]
+        else:
+            name = remain.split(':')[0]
+
+        indent = len(spaces)
+        if indent in self.classes_parent:
+            self.classes_parent[indent].append(name)
+        else:
+            self.classes_parent[indent] = [name]
+
+    def _parse_function(self, line):
+        splits = line.split('def ')
+        
+        indent = len(splits[0])
+
+        remain = splits[1].split('(')
+        name = remain[0]
+        value_str = remain[1].split(')')[0]
+
         if ' ' in value_str:
             values = value_str.split(', ')
         else:
             values = value_str.split(',')
 
+        if indent == 0:
+            self.functions_parent['__file__'].append(name)
+            self.functions.append(name)
+        else:
+            ind = indent - self.indent
+            if ind in self.classes_parent:
+                class_name = self.classes_parent[ind][-1]
+                if class_name in self.functions_parent:
+                    self.functions_parent[class_name].append(name)
+                    self.functions.append(name)
+                else:
+                    self.functions_parent[class_name] = [name]
+                    self.functions.append(name)
         return name, values
--- vocabulary/types/user_defined.py
+++ vocabulary/types/user_defined.py
@@ -3,6 +3,12 @@
 
 template_user_functions = ['foo', 'bar', 'baz']
 
+template_classes = ['', '', '']
+
 template_user_functions_contents = {'foo': [], 'bar': [], 'baz': []}
 
 template_user_func_replaces = {'foo': '', 'bar': '', 'baz': ''}
+
+{'pan': ['foo', 'bar', 'baz']}
+
+{'pan': {'foo': []}}
