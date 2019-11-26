9d537a3da286e578af920b0646f1381d49f23157
amoh-godwin <amohgyebigodwin@gmail.com>
2019-11-26 14:46:06 +0000

    use line by line reading instead of whole content read

--- vocabulary/py_voc.py
+++ vocabulary/py_voc.py
@@ -13,6 +13,11 @@ class Pyvoc():
         self.base_types_dict = base_types_dict
         self.operand_types = ['+', '-', '=', '/', '*']
         self.replace_processes = ['base']
+        self.lines = []
+        self.r_lines_len = 0
+        self.curr_type = ''
+        self.curr_type_name = ''
+        self.curr_index = 0
 
     def start(self):
 
@@ -21,42 +26,70 @@ class Pyvoc():
         self.content = user_def.start()
         self._sanitise_quotes()
 
-        # Replaces
-        for var in self.replace_processes:
-            self._replace(var)
+        self.main_parser(self.content)
 
-        # Underline unfound
-        self._mark_unfound()
+        self.content = self.rebuild_content()
+        print('32: ', self.content)
 
         print(self.content)
 
         return self.content
 
-    def _mark_unfound(self):
+    def main_parser(self, content):
+
+        lines = content.split('\r\n')
+        self.r_lines_len = len(lines)
+
+        no = -1
+        for line in lines:
+            no += 1
+            line = self._start_replace_processes(line)
+            print('45: ', line)
+            # Underline unfound
+            line = self._mark_unfound(line, no)
+            print('49: ', line)
+
+            self.lines.append(line)
+
+    def rebuild_content(self):
+
+        string = ''
+        print('56: ', self.lines)
+        for line in self.lines:
+            print('58: ', line)
+            string += line + '\r\n'
+
+        return string
+
+    def _start_replace_processes(self, line):
+
+        # Replaces
+        for var in self.replace_processes:
+            line = self._replace(var, line)
+        return line
+
+    def _mark_unfound(self, line, no):
 
         word_splits = []
-        lines_splits = self.content.split('\r\n')
-        lines_len = len(lines_splits)
-        no = -1
-        for line in lines_splits:
-            no += 1
-            # Find if line contains just spaces
-            founds = re.findall("[A-Za-z0-9`~!@#$%^&*\(\)\[\]-{}_=+/?<,.|>]*", line)
-            found = str(founds).replace(', ', '').replace("'", "")
-            if found == '[]':
-                # Empty
-                if (no+1) < lines_len:
-                    line = '\u2029'
-                else:
-                    line = ''
+        # Find if line contains just spaces
+        founds = re.findall("[A-Za-z0-9`~!@#$%^&*\(\)\[\]-{}_=+/?<,.|>]*", line)
+        found = str(founds).replace(', ', '').replace("'", "")
+        if found == '[]':
+            # Empty
+            if (no+1) < self.r_lines_len:
+                line = '\u2029'
             else:
-                line += '\u2029'
-            splits = line.split(" ")
-            word_splits.extend(splits)
+                line = ''
+        else:
+            line += '\u2029'
+        splits = line.split(" ")
+        word_splits.extend(splits)
         word_splits_s = word_splits
 
         no = -1
         content = ""
+
+        # Start with the Marking of the unfound
         for word in word_splits:
             no += 1
             if word.startswith('<span>'):
@@ -74,6 +107,7 @@ class Pyvoc():
                 stat = '<span style="color: red">' + word + '</span>'
                 word_splits_s[no] = stat
 
+        # recompose back into a line
         for each in word_splits_s:
             if each == '\u2029':
                 content += '\u2029'
@@ -84,9 +118,10 @@ class Pyvoc():
             else:
                 content += each + "&nbsp;"
 
+        return content
         self.content = content
 
-    def _replace(self, var):
+    def _replace(self, var, line):
 
         if var == 'base':
             main_var = self.base_types
@@ -95,15 +130,17 @@ class Pyvoc():
             main_func_dict = base_func_dict
 
         # variable replacement
-        self._replace_space_var(main_var, main_dict)
+        self._replace_space_var(main_var, main_dict, line)
 
         # function replacement
         for y in main_func:
-            if y in self.content:
-                self.content = self.content.replace(y, main_func_dict[y])
+            if y in line:
+                line = line.replace(y, main_func_dict[y])
 
-    def _replace_space_var(self, var, var_dict):
-        splits = self.content.split(' ')
+        return line
+
+    def _replace_space_var(self, var, var_dict, line):
+        splits = line.split(' ')
 
         for x in var:
             if x in splits:
@@ -116,7 +153,8 @@ class Pyvoc():
             string += a + " "
 
         # And back to contents
-        self.content = string[:-1]
+        line = string[:-1]
+        return line
 
     def _sanitise_quotes(self):
         lines = self.content.split('\r\n')
