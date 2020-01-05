6f04c7897a1ff5d7f57d599b96f0abffa2610875
amoh-godwin <amohgyebigodwin@gmail.com>
2020-01-05 21:02:24 +0000

    create an escape for unicode

--- vocabulary/misc/misc.py
+++ vocabulary/misc/misc.py
@@ -22,6 +22,22 @@ def add_splitter(splits, splitter):
 
     return restruct
 
+def escape_unicode(line):
+    unicode = {'&nbsp;': '_space__uni_code'}
+    
+    for code in unicode:
+        line = line.replace(code, unicode[code])
+
+    return line
+
+def put_back_unicode(line):
+    words = {'_space__uni_code': '&nbsp;'}
+    
+    for word in words:
+        line = line.replace(word, words[word])
+
+    return line
+    
 
 def fix_span_stat(old_stat):
     # fix escape for less and greater than symbols
