f0940c94120cda1c20c43b6c72d3485a1843036a
amoh-godwin <amohgyebigodwin@gmail.com>
2020-01-06 17:47:50 +0000

    Update base.py

--- vocabulary/types/base.py
+++ vocabulary/types/base.py
@@ -12,15 +12,7 @@ base_functions = ['print(', 'list(', 'str(', 'bytes(', 'dict(', 'set(', 'tuple('
 
 base_operand_repl = "[[span]]{}[[jeridespan]]"
 
-base_types_repl = "<span>{}</span>"
-
-base_types_dict = {'if': "<span>if</span>", 'property': "<span>property</span>",
-                   'elif': "<span>elif</span>", 'else': "<span>else</span>",
-                   "for": "<span>for</span>", 'while': "<span>while</span>",
-                   'class': "<span>class</span>", 'def': "<span>def</span>",
-                   'with': "<span>with</span>", 'as': "<span>as</span>",
-                   'in': "<span>in</span>", 'import': "<span>import</span>",
-                   'from': "<span>from</span>", 'and': "<span>and</span>"}
+base_types_repl = "[[span]]{}[[jeridespan]]"
 
 base_func_dict = {'print(': "<span>print</span>(", 'list(': "<span>list</span>(",
                   'str(': "<span>str</span>(", 'bytes(': "<span>bytes</span>(",
